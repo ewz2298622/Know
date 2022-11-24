2b0247d2d6771c95a390e03f766c592aa2359225
缘 <67059060+ewz2298621@users.noreply.github.com>
2022-11-24 22:18:58 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Dy6080.java
+++ app/src/main/java/com/github/catvod/spider/Dy6080.java
@@ -391,19 +391,23 @@ public class Dy6080 extends Spider {
                         Elements script = doc.select("body>script");
                         for (int j = 0; j < script.size(); j++) {
                           String content = script.get(j).html().trim();
-                        if (content.contains("var config =")){
-                            Matcher matcher1 = urlt.matcher(content);
-                            if (!matcher1.find())
-                                continue;
-                            Matcher matcher2 = token.matcher(content);
-                            if (!matcher2.find())
-                                continue;
-                            Matcher matcher3 = vkey.matcher(content);
+                          System.out.println("nr" + content);
+                          if (content.contains("var config =")) {
+                          Matcher matcher1 = urlt.matcher(content);
+                          if (!matcher1.find())
+                             continue;
+                          System.out.println("urt" + matcher1);
+                          Matcher matcher2 = time.matcher(content);
+                          if (!matcher2.find())
+                             continue;
+                            Matcher matcher3 = key.matcher(content);
                             if (!matcher3.find())
-                                continue;
+                               continue;
+                            System.out.println("urt1" + matcher3);
                             String video_url = matcher1.group(1);
-                            String video_token = matcher2.group(1);
-                            String video_key = matcher3.group(1);                          
+                            System.out.println("vr" +video_url);
+                            String key = matcher3.group(1);
+                            System.out.println("k" + key);                          
                         //    String video_sign= "F4penExTGogdt6U8" ;
                             String video_tm = String.valueOf(System.currentTimeMillis()/ 1000);
                             HashMap hashMap = new HashMap();
