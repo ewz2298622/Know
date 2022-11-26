94a401de050c8640c6182188da24febecb3d0c8a
缘 <67059060+ewz2298621@users.noreply.github.com>
2022-11-26 19:27:34 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Dy6080.java
+++ app/src/main/java/com/github/catvod/spider/Dy6080.java
@@ -264,46 +264,15 @@ public class Dy6080 extends Spider {
     public String detailContent(List<String> ids) {
         try {
             // 视频详情url
-            String url = siteUrl + "/vplay/" + ids.get(0) + ".html";
+            String url = siteUrl + "/video/" + ids.get(0) + ".html";
             Document doc = Jsoup.parse(OkHttpUtil.string(url, getHeaders(url)));
             JSONObject result = new JSONObject();
             JSONObject vodList = new JSONObject();
-
             // 取基本数据
-            String cover = doc.selectFirst("div.module-item-pic img").attr("data-original");
-            String title = doc.selectFirst("div.video-info-header h1").text();
-            String category = "", area = "", year = "", remark = "", director = "", actor = "", desc = "";
-    
-            Elements span_text_muted = doc.select("div.video-info-main div.video-info-items span");
-           year = doc.selectFirst("div.module-info-tag > div > a").text();
-            desc = doc.selectFirst("div.video-info-content").text().trim();
-            for (int i = 0; i < span_text_muted.size(); i++) {
-                Element text = span_text_muted.get(i);
-                String info = text.text();
-                if (info.equals("分类：")) {
-                    category = text.nextElementSibling().text();
-                } else if (info.equals("上映：")) {
-                    year = text.nextElementSibling().text();
-                } else if (info.equals("地区：")) {
-                    area = text.nextElementSibling().text();
-                } else if (info.equals("更新：")) {
-                    remark = text.nextElementSibling().text();
-                } else if (info.equals("导演：")) {
-                    List<String> directors = new ArrayList<>();
-                    Elements aa = text.parent().select("a");
-                    for (int j = 0; j < aa.size(); j++) {
-                        directors.add(aa.get(j).text());
-                    }
-                    director = TextUtils.join(",", directors);
-                } else if (info.equals("主演：")) {
-                    List<String> actors = new ArrayList<>();
-                    Elements aa = text.parent().select("a");
-                    for (int j = 0; j < aa.size(); j++) {
-                        actors.add(aa.get(j).text());
-                    }
-                    actor = TextUtils.join(",", actors);
-                }
-            }
+            String cover = doc.selectFirst("div.module-item-pic > img").attr("data-src");
+            String title = doc.selectFirst("div.video-info-header > h1.page-title").text();
+            String desc = doc.select("div.video-info-content span").text();
+            String category = "", area = "", year = "", remark = "", director = "", actor = "";
 
             vodList.put("vod_id", ids.get(0));
             vodList.put("vod_name", title);
@@ -315,7 +284,6 @@ public class Dy6080 extends Spider {
             vodList.put("vod_actor", actor);
             vodList.put("vod_director", director);
             vodList.put("vod_content", desc);
-            //System.out.println(vodList.toString());
             Map<String, String> vod_play = new TreeMap<>(new Comparator<String>() {
                 @Override
                 public int compare(String o1, String o2) {
@@ -333,7 +301,6 @@ public class Dy6080 extends Spider {
                     return 1;
                 }
             });
-
             // 取播放列表数据
             Elements sources = doc.select("div.module-tab-content div span");
             Elements sourceList = doc.select("div.module-player-list");
